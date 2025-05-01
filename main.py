939752f6a67b4fed69d4ad5202ef5fe3a23ea745
Roman Kalmykow <snikasipoin@gmail.com>
2025-05-02 02:52:21 +0600

    порт без переменных

--- main.py
+++ main.py
@@ -221,13 +221,8 @@ def upload_file():
         return jsonify({"error": f"Ошибка обработки: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(
-        debug=False,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000))  # fallback если переменной нет
-    )
-
-
+    port = 8080
+    app.run(debug=True,host='0.0.0.0',port=port)
 
 
 
